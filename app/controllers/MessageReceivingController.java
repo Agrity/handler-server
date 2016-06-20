a9ba6e5c5d785f35d2ca892b7cbeef7a0af0e53c
Ryan Davies <ryscot@gmail.com>
2016-06-20 16:34:42 -0700

    Routing Done

---
+++ app/controllers/MessageReceivingController.java
@@ -0,0 +1,42 @@
+package controllers;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.inject.Inject;
+
+import java.util.List;
+
+import models.Offer;
+
+import play.mvc.BodyParser;
+import play.mvc.Controller;
+import play.mvc.Result;
+
+import services.OfferService;
+import services.messaging.offer.OfferMessageService;
+import services.parsers.OfferJsonParser;
+
+public class MessageReceivingController extends Controller {
+
+  private static Integer numResponses = 0;
+
+  private final OfferService offerService;
+  private final OfferMessageService offerMessageService;
+
+  @Inject
+  public MessageReceivingController(OfferService offerService, OfferMessageService offerMessageService) {
+    this.offerService = offerService;
+    this.offerMessageService = offerMessageService;
+  }
+
+  public Result numberTwilioResponses() {
+    return ok("Number Responses Recieved: " + numResponses);
+  }
+
+  public Result receiveTwilioResponse() {
+    numResponses++;
+    return ok("Response Received");
+  }
+
+}
