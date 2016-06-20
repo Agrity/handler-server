7b76d1504787c3a9bb9e490900ee1c20eb0364d3
twfabdel <twfabdel@gmail.com>
2016-06-20 16:22:16 -0700

    Test for expiration

--- test/servies/impl/WaterfallServiceTest.java
+++ test/servies/impl/WaterfallServiceTest.java
@@ -1,7 +1,8 @@
-package models;
+package servies.impl;
 
 import static org.hamcrest.CoreMatchers.anyOf;
 import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.not;
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.CoreMatchers.notNullValue;
 import static org.junit.Assert.assertThat;
@@ -12,9 +13,17 @@ import org.junit.Test;
 import java.time.LocalDate;
 import java.time.Duration;
 import java.time.Month;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.lang.Thread;
 
 import models.Almond.AlmondVariety;
+import models.Grower;
+import models.Handler;
+import models.Offer;
+
+import services.WaterfallService;
 
 import test_helpers.EbeanTest;
 
@@ -23,16 +32,20 @@ import static org.mockito.Mockito.*;
 import com.avaje.ebean.Ebean;
 import com.google.common.collect.ImmutableList;
 
-import models.WaterfallService;
+
+//import services.WaterfallService;
 
 
 
-public class WaterfallServiceTest extends Ebean {	
+public class WaterfallServiceTest extends EbeanTest {	
 
 	private static final Handler UNUSED_HANDLER = new Handler("Test Company");
 	private static final List<Grower> UNUSED_GROWERS = new ImmutableList.Builder<Grower>()
 		.add(new Grower(UNUSED_HANDLER, "F1", "L1"))
 		.add(new Grower(UNUSED_HANDLER, "F2", "L2"))
+    .add(new Grower(UNUSED_HANDLER, "F3", "L3"))
+    .add(new Grower(UNUSED_HANDLER, "F4", "L4"))
+    .add(new Grower(UNUSED_HANDLER, "F5", "L5"))
     .build();
 
 	private static final AlmondVariety UNUSED_VARIETY = AlmondVariety.NONPAREIL;
@@ -62,11 +75,21 @@ public class WaterfallServiceTest extends Ebean {
     assertThat(offer, is(notNullValue()));
     saveModel(offer);
 
-    WaterfallService wservice = new wservice(offer, Duration.ofSeconds(5));
+    WaterfallService wservice = new WaterfallService(offer, Duration.ofMillis(100));
 
+    int index = 0;
     while(wservice.process()) {
-      
+      List<Grower> g = wservice.getCurrentGrowers();
+      assertThat(g.get(0), is(equalTo(UNUSED_GROWERS.get(index))));
+      try {
+        Thread.sleep(110);
+      } catch(InterruptedException ex) {
+        Thread.currentThread().interrupt();
+      }
+      index += 1;
     }
+
+    assertThat(wservice.getCurrentGrowers().size(), is(0));
 
   }
 
