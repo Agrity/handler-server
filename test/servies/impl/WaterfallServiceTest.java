1e947c964b1a862365e2fa57bd22bc18a8a56c3c
twfabdel <twfabdel@gmail.com>
2016-06-20 21:08:09 -0700

    added more test scripts

--- test/servies/impl/WaterfallServiceTest.java
+++ test/servies/impl/WaterfallServiceTest.java
@@ -22,6 +22,8 @@ import models.Almond.AlmondVariety;
 import models.Grower;
 import models.Handler;
 import models.Offer;
+import models.OfferResponse;
+import models.OfferResponse.ResponseStatus;
 
 import services.WaterfallService;
 
@@ -33,8 +35,6 @@ import com.avaje.ebean.Ebean;
 import com.google.common.collect.ImmutableList;
 
 
-//import services.WaterfallService;
-
 
 
 public class WaterfallServiceTest extends EbeanTest {	
@@ -45,7 +45,6 @@ public class WaterfallServiceTest extends EbeanTest {
 		.add(new Grower(UNUSED_HANDLER, "F2", "L2"))
     .add(new Grower(UNUSED_HANDLER, "F3", "L3"))
     .add(new Grower(UNUSED_HANDLER, "F4", "L4"))
-    .add(new Grower(UNUSED_HANDLER, "F5", "L5"))
     .build();
 
 	private static final AlmondVariety UNUSED_VARIETY = AlmondVariety.NONPAREIL;
@@ -78,19 +77,124 @@ public class WaterfallServiceTest extends EbeanTest {
     WaterfallService wservice = new WaterfallService(offer, Duration.ofMillis(100));
 
     int index = 0;
+    int counter = 0;
     while(wservice.process()) {
       List<Grower> g = wservice.getCurrentGrowers();
       assertThat(g.get(0), is(equalTo(UNUSED_GROWERS.get(index))));
       try {
-        Thread.sleep(110);
+        Thread.sleep(55);
       } catch(InterruptedException ex) {
         Thread.currentThread().interrupt();
       }
-      index += 1;
+      counter += 1;
+      if(counter % 2 == 0) index += 1;
     }
 
     assertThat(wservice.getCurrentGrowers().size(), is(0));
 
   }
+
+  @Test
+  public void testAllRejected() {
+    Offer offer
+      = new Offer(
+          UNUSED_HANDLER,
+          UNUSED_GROWERS,
+          UNUSED_VARIETY,
+          UNUSED_POUNDS,
+          UNUSED_PRICE,
+          UNUSED_DATE,
+          UNUSED_COMMENT);
+
+    assertThat(offer, is(notNullValue()));
+    saveModel(offer);
+
+    WaterfallService wservice = new WaterfallService(offer, Duration.ofMillis(30));
+
+    int index = 0;
+    while(wservice.process()) {
+      List<Grower> g = wservice.getCurrentGrowers();
+      OfferResponse or = offer.getGrowerOfferResponse(g.get(0).getId());
+      assertThat(g.get(0), is(equalTo(UNUSED_GROWERS.get(index))));
+      or.setResponseStatus(ResponseStatus.REJECTED);
+      index++;
+    }
+    assertThat(wservice.getCurrentGrowers().size(), is(0));
+  
+  }
+
+  @Test
+  public void testFirstAccepted() {
+    Offer offer
+      = new Offer(
+          UNUSED_HANDLER,
+          UNUSED_GROWERS,
+          UNUSED_VARIETY,
+          UNUSED_POUNDS,
+          UNUSED_PRICE,
+          UNUSED_DATE,
+          UNUSED_COMMENT);
+
+    assertThat(offer, is(notNullValue()));
+    saveModel(offer);
+
+    WaterfallService wservice = new WaterfallService(offer, Duration.ofMillis(300));
+
+    int index = 0;
+    while(wservice.process()) {
+      List<Grower> g = wservice.getCurrentGrowers();
+      OfferResponse or = offer.getGrowerOfferResponse(g.get(0).getId());
+      assertThat(g.get(0), is(equalTo(UNUSED_GROWERS.get(index))));
+      or.setResponseStatus(ResponseStatus.ACCEPTED);
+      index++;
+    }
+    assertThat(wservice.getCurrentGrowers().size(), is(4));
+
+  }
+
+  @Test
+  public void test2Reject3rdAccepts() {
+    Offer offer
+      = new Offer(
+          UNUSED_HANDLER,
+          UNUSED_GROWERS,
+          UNUSED_VARIETY,
+          UNUSED_POUNDS,
+          UNUSED_PRICE,
+          UNUSED_DATE,
+          UNUSED_COMMENT);
+
+    assertThat(offer, is(notNullValue()));
+    saveModel(offer);
+
+    WaterfallService wservice = new WaterfallService(offer, Duration.ofMillis(1000));
+    assertThat(wservice.getCurrentGrowers().size(), is(4));
+
+    int index = 0;
+    while(wservice.process()) {
+      List<Grower> g = wservice.getCurrentGrowers();
+      assertThat(g.get(0), is(equalTo(UNUSED_GROWERS.get(index))));
+      
+      if(index == 2) {
+        OfferResponse or = offer.getGrowerOfferResponse(g.get(0).getId());
+        or.setResponseStatus(ResponseStatus.ACCEPTED);
+      } else if (index < 2) {
+        OfferResponse or = offer.getGrowerOfferResponse(g.get(0).getId());
+        or.setResponseStatus(ResponseStatus.REJECTED);
+      }
+      
+      index++;
+    }
+    assertThat(wservice.getCurrentGrowers().size(), is(2));
+
+  }
+
+
+
+
+
+
+
+
 
 }
