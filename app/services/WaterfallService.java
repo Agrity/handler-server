fe7d5d6adcb9eda279750f4b08d35df3b3563885
twfabdel <twfabdel@gmail.com>
2016-06-21 15:03:41 -0700

    code written

--- app/services/WaterfallService.java
+++ app/services/WaterfallService.java
@@ -23,17 +23,17 @@ public class WaterfallService {
   private Cancellable cancellable;
 
 //	private LocalDateTime expirationTime;
-	private List<Grower> growers;
+  private List<Grower> growers;
 
-	public WaterfallService(Offer offer, Duration delay) {
-		this.offer = offer;
-		this.delay = delay;
+  public WaterfallService(Offer offer, Duration delay) {
+    this.offer = offer;
+    this.delay = delay;
 //		expirationTime = offer.getCreatedAt().plus(delay);
-		this.growers = new ArrayList<Grower>(offer.getAllGrowers());
+    this.growers = new ArrayList<Grower>(offer.getAllGrowers());
 
     cancellable = scheduleTimer();
 
-	}
+  }
 
   private Cancellable scheduleTimer() {
     //alert farmer 0 of offer
@@ -50,31 +50,30 @@ public class WaterfallService {
     OfferResponse response = offer.getGrowerOfferResponse(growers.get(0).getId());
     ResponseStatus status = response.getResponseStatus();
 
-    switch(status) {
-    case ACCEPTED:
-      accept(); break;
-    case REJECTED:
-      reject(); break;
-    case REQUEST_CALL:
-       //return something?
-    default:
-      //alert farmer 0 of time expired
-      growers.remove(0);
-      if(growers.size() != 0) {
-        cancellable = scheduleTimer();
-      } else {
-        
-      }
-   }
+    if(status == ResponseStatus.ACCEPTED) {
+      accept();
+    } else moveToNext();
 
+  }
+
+  private void moveToNext() {
+    //alert farmer 0 of time expired
+    growers.remove(0);
+    if(growers.size() != 0) {
+      cancellable = scheduleTimer();
+    } else {
+      //offer.close();
+    }
   }
 
   public void accept() {
     cancellable.cancel();
+    //offer.close();
   }
 
   public void reject() {
     //TODO: remove front and reschedule
+    moveToNext();
   }
 
 
@@ -101,14 +100,14 @@ public class WaterfallService {
 	// 		default:
 	// 	}
 	// 	return true;
-		
+
 	// }
 
 	// private boolean moveToNext() {
 	// 	//Send message to 0 that their time has expired
 	// 	growers.remove(0);
 	// 	if(growers.size() == 0) return false;
-		
+
 	// 	expirationTime = LocalDateTime.now().plus(delay);
 	// 	//Send message to new 0 for the offer
 	// 	return true;
