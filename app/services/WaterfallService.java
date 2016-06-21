99cc573c1dab2ea8902ffe18387eef5e3ffc2d5c
twfabdel <twfabdel@gmail.com>
2016-06-21 15:51:36 -0700

    cleaned up.

--- app/services/WaterfallService.java
+++ app/services/WaterfallService.java
@@ -20,13 +20,11 @@ public class WaterfallService {
 	private final Duration delay;
   private Cancellable cancellable;
 
-//	private LocalDateTime expirationTime;
   private List<Grower> growers;
 
   public WaterfallService(Offer offer, Duration delay) {
     this.offer = offer;
     this.delay = delay;
-//		expirationTime = offer.getCreatedAt().plus(delay);
     this.growers = new ArrayList<Grower>(offer.getAllGrowers());
 
     cancellable = scheduleTimer();
@@ -71,7 +69,6 @@ public class WaterfallService {
   }
 
   public void reject() {
-    
     cancellable.cancel();
     moveToNext();
   }
@@ -79,44 +76,5 @@ public class WaterfallService {
   public List<Grower> getCurrentGrowers() {
     return growers;
   }
-
-
-
- //  public boolean process() {
-	// 	LocalDateTime current = LocalDateTime.now();	
-	// 	if(current.isAfter(expirationTime)) {
-	// 		// Time has passed, remove the front and return false if the list is empty
-	// 		return moveToNext();
-	// 	} 
-
-	// 	OfferResponse response = offer.getGrowerOfferResponse(growers.get(0).getId());
-	// 	ResponseStatus status = response.getResponseStatus();
-
-	// 	switch(status) {
-	// 		//set response status for grower here?
-	// 		case ACCEPTED:
-	// 		 return false;
-	// 		case REJECTED:
-	// 		 return moveToNext();
-	// 		case REQUEST_CALL:
-	// 			//return something?
-	// 		default:
-	// 	}
-	// 	return true;
-
-	// }
-
-	// private boolean moveToNext() {
-	// 	//Send message to 0 that their time has expired
-	// 	growers.remove(0);
-	// 	if(growers.size() == 0) return false;
-
-	// 	expirationTime = LocalDateTime.now().plus(delay);
-	// 	//Send message to new 0 for the offer
-	// 	return true;
-	// }
-
-
-
 
 }
