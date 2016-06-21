dd6d871eb68d0dabbd0e16850d5daff45132d0ae
twfabdel <twfabdel@gmail.com>
2016-06-21 14:59:10 -0700

    changing waterfall to Akka scheduler

--- app/services/WaterfallService.java
+++ app/services/WaterfallService.java
@@ -8,62 +8,115 @@ import models.OfferResponse.ResponseStatus;
 
 
 import java.time.Duration;
-import java.time.LocalDateTime;
+//import java.time.LocalDateTime;
 
+import akka.actor.Cancellable;
+import scala.concurrent.duration.FiniteDuration;
+import akka.actor.UntypedActor;
+import java.util.concurrent.TimeUnit;
+import play.libs.Akka;
 
 public class WaterfallService {
 
 	private final Offer offer;
 	private final Duration delay;
+  private Cancellable cancellable;
 
-	private LocalDateTime expirationTime;
+//	private LocalDateTime expirationTime;
 	private List<Grower> growers;
 
 	public WaterfallService(Offer offer, Duration delay) {
 		this.offer = offer;
 		this.delay = delay;
-		expirationTime = offer.getCreatedAt().plus(delay);
+//		expirationTime = offer.getCreatedAt().plus(delay);
 		this.growers = new ArrayList<Grower>(offer.getAllGrowers());
 
+    cancellable = scheduleTimer();
+
 	}
 
-	public boolean process() {
-		LocalDateTime current = LocalDateTime.now();	
-		if(current.isAfter(expirationTime)) {
-			// Time has passed, remove the front and return false if the list is empty
-			return moveToNext();
-		} 
-
-		OfferResponse response = offer.getGrowerOfferResponse(growers.get(0).getId());
-		ResponseStatus status = response.getResponseStatus();
-
-		switch(status) {
-			//set response status for grower here?
-			case ACCEPTED:
-			 return false;
-			case REJECTED:
-			 return moveToNext();
-			case REQUEST_CALL:
-				//return something?
-			default:
-		}
-		return true;
-		
-	}
-
-	private boolean moveToNext() {
-		//Send message to 0 that their time has expired
-		growers.remove(0);
-		if(growers.size() == 0) return false;
-		
-		expirationTime = LocalDateTime.now().plus(delay);
-		//Send message to new 0 for the offer
-		return true;
-	}
-
-  public List<Grower> getCurrentGrowers() {
-    return growers;
+  private Cancellable scheduleTimer() {
+    //alert farmer 0 of offer
+    return Akka.system().scheduler().scheduleOnce(FiniteDuration.create(delay.toMillis(), TimeUnit.MILLISECONDS), 
+      new Runnable() {
+        @Override
+        public void run() {
+          process();
+        }
+      }, Akka.system().dispatcher());
   }
+
+  private void process() {
+    OfferResponse response = offer.getGrowerOfferResponse(growers.get(0).getId());
+    ResponseStatus status = response.getResponseStatus();
+
+    switch(status) {
+    case ACCEPTED:
+      accept(); break;
+    case REJECTED:
+      reject(); break;
+    case REQUEST_CALL:
+       //return something?
+    default:
+      //alert farmer 0 of time expired
+      growers.remove(0);
+      if(growers.size() != 0) {
+        cancellable = scheduleTimer();
+      } else {
+        
+      }
+   }
+
+  }
+
+  public void accept() {
+    cancellable.cancel();
+  }
+
+  public void reject() {
+    //TODO: remove front and reschedule
+  }
+
+
+
+
+ //  public boolean process() {
+	// 	LocalDateTime current = LocalDateTime.now();	
+	// 	if(current.isAfter(expirationTime)) {
+	// 		// Time has passed, remove the front and return false if the list is empty
+	// 		return moveToNext();
+	// 	} 
+
+	// 	OfferResponse response = offer.getGrowerOfferResponse(growers.get(0).getId());
+	// 	ResponseStatus status = response.getResponseStatus();
+
+	// 	switch(status) {
+	// 		//set response status for grower here?
+	// 		case ACCEPTED:
+	// 		 return false;
+	// 		case REJECTED:
+	// 		 return moveToNext();
+	// 		case REQUEST_CALL:
+	// 			//return something?
+	// 		default:
+	// 	}
+	// 	return true;
+		
+	// }
+
+	// private boolean moveToNext() {
+	// 	//Send message to 0 that their time has expired
+	// 	growers.remove(0);
+	// 	if(growers.size() == 0) return false;
+		
+	// 	expirationTime = LocalDateTime.now().plus(delay);
+	// 	//Send message to new 0 for the offer
+	// 	return true;
+	// }
+
+ //  public List<Grower> getCurrentGrowers() {
+ //    return growers;
+ //  }
 
 
 
