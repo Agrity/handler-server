d183eec872d887f81fa5305bc4d6d9ecb25c2946
Brian Rossi <brossi15621@gmail.com>
2016-06-22 12:44:27 -0700

    trying xml doc

--- app/controllers/MessageReceivingController.java
+++ app/controllers/MessageReceivingController.java
@@ -20,6 +20,15 @@ import services.parsers.OfferJsonParser;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
 
+import org.w3c.dom.Document;
+import org.w3c.dom.DocumentType;
+import org.w3c.dom.Entity;
+import org.w3c.dom.NamedNodeMap;
+import org.w3c.dom.Node;
+
+import javax.xml.xpath.XPath;
+import javax.xml.xpath.XPathFactory;
+
 public class MessageReceivingController extends Controller {
 
   private static Integer numResponses = 0;
@@ -39,22 +48,37 @@ public class MessageReceivingController extends Controller {
 
   public Result receiveTwilioResponse() {
     numResponses++;
-    JsonNode data = request().body().asJson();
+    Document dom = request().body().asXml();
+    if (dom == null) {
+      return badRequest("Expecting Xml data");
+    }
+    return ok("Have a non-null XML doc");
+    //XPath xPath = XPathFactory.newInstance().newXPath();
+    //String text = xPath.getTextContent("//body", dom);
+    //if (text == null) {
+    //  return badRequest("Missing parameter [text]");
+    //} else {
+    //  return ok(text);
+    //}
+
+
+
+  /*  JsonNode data = request().body().asJson();
 
     if (data == null) {
-      // TODO Change to Valid Error JSON
-      return ok("Expecting Some Data.\n"); /* bad request? */
+      return ok("Expecting Some Data.\n"); 
     }
 
     String num = data.findValue("From").textValue();
     if (num == null) {
-      return ok("From parameter is empty."); /* bad request? */
+      return ok("From parameter is empty."); 
     }
     String text = data.findValue("Body").textValue();
     if (text == null) {
-      return ok("Body parameter is empty."); /* bad request? */
+      return ok("Body parameter is empty."); \
     }
     return ok(num + "\n" + text);
+  */
   }
 
     
