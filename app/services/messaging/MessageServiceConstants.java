12369164b372cf8be24394dbffac02d584603ee5
Brian Rossi <brossi15621@gmail.com>
2016-06-24 16:47:30 -0700

    test twilio account being used

--- app/services/messaging/MessageServiceConstants.java
+++ app/services/messaging/MessageServiceConstants.java
@@ -47,9 +47,10 @@ public abstract class MessageServiceConstants {
   public static class TwilioFields {
     private static final String ACCOUNT_SID = "AC486e1d38a0597859c70f32589ea3ab1a";
     private static final String AUTH_TOKEN = "8e930e13c7f8f31606ac3089baf8adec";
+    private static final String TEST_AUTH_TOKEN = "4d6520f79e75c93c4d80ec72a68fd6f1";
     private static final String TWILIO_NUMBER = "+15592057098";
     
-    private static final TwilioRestClient client = new TwilioRestClient(ACCOUNT_SID, AUTH_TOKEN);
+    private static final TwilioRestClient client = new TwilioRestClient(ACCOUNT_SID, TEST_AUTH_TOKEN);
     private static final Account account = client.getAccount();
     private static final MessageFactory messageFactory = account.getMessageFactory();
 
