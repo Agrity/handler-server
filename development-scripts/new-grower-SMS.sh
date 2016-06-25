0d8b14e05175d62fe8c921fe239946194ae88a6b
Brian Rossi <brossi15621@gmail.com>
2016-06-24 18:14:32 -0700

    fixed phone numbers dilemma?

--- development-scripts/new-grower-SMS.sh
+++ development-scripts/new-grower-SMS.sh
@@ -4,7 +4,7 @@ if [[ -z $1 ]]; then
   curl \
     --header "Content-type: application/json" \
     --request POST \
-    --data '{ "handler_id" : 1, "first_name" : "Brian", "last_name" : "Rossi", "email_addresses" :  [ "brossi15621@gmail.com" ], "PHONE_NUMBERS" : [ "+18155926350" ] }' \
+    --data '{ "handler_id" : 1, "first_name" : "Brian", "last_name" : "Rossi", "email_addresses" :  [ "brossi15621@gmail.com" ], "phone_numbers" : [ "+18155926350" ] }' \
     radiant-cove-44181.herokuapp.com/growers
 else
   echo "ERROR: do not use any arguements."
