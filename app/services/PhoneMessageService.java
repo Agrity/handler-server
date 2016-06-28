016892437dc8187e64a1b82a660387cd4182cab2
Brian Rossi <brossi15621@gmail.com>
2016-06-28 15:20:10 -0700

    phone number format verification

--- app/services/PhoneMessageService.java
+++ app/services/PhoneMessageService.java
@@ -10,20 +10,20 @@ public class PhoneMessageService {
   private static final String formatExample = "Example phone number: +1650123456";
 
   public static boolean verifyPhoneNumber(String phoneNumber) {
-    // if (phoneNumber.length() != 12) {
-    //   Logger.error(phoneNumber + " has an invalid phone number length. " + formatExample);
-    //   return false;
-    // }
+     if (phoneNumber.length() != 12) {
+       Logger.error(phoneNumber + " has an invalid phone number length. " + formatExample);
+       return false;
+     }
 
-    // if (phoneNumber.charAt(0) != '+') {
-    //   Logger.error(phoneNumber + " does not begin with leading '+'. " + formatExample);
-    //   return false;
-    // }
+     if (phoneNumber.charAt(0) != '+') {
+       Logger.error(phoneNumber + " does not begin with leading '+'. " + formatExample);
+       return false;
+     }
 
-    // if (phoneNumber.charAt(1) != '1') {
-    //   Logger.error(phoneNumber + " does not include a trunk prefix of 1. " + formatExample);
-    // 	return false;
-    // }
+     if (phoneNumber.charAt(1) != '1') {
+       Logger.error(phoneNumber + " does not include a trunk prefix of 1. " + formatExample);
+     	return false;
+     }
     
     return true;
   }
