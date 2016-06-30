3c9cd2fa3f5718cd44214a657ae791fef060dc15
Ryan Davies <ryscot@gmail.com>
2016-06-30 11:11:26 -0700

    Version Revision and Test Scripts

---
+++ test_scripts/init.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+./new-handler.sh "Admin Company" "admin@admin.com" "admin"
+
+./new-grower.sh
+
+./new-offer.sh
---
+++ test_scripts/new-grower.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+if [[ -z $1 ]]; then
+  curl \
+    --header "Content-type: application/json" \
+    --header "X-ADMIN-TOKEN: development-use-only" \
+    --request POST \
+    --data '{ "handler_id" : 1, "first_name" : "F_NAME", "last_name" : "L_NAME", "email_addresses" :  [] }' \
+    http://server.test.agrity.net/admin/growers
+else
+  echo "ERROR: do not use any arguements."
+fi
+
+
