c046686587f540f36e49591ddbe56321d98748fb
Brian Rossi <brossi15621@gmail.com>
2016-07-08 16:16:43 -0700

    almond size

--- app/models/BaseBid.java
+++ app/models/BaseBid.java
@@ -8,15 +8,34 @@ import models.Almond.AlmondVariety;
 @MappedSuperclass
 public abstract class BaseBid extends BaseModel {
 
+/* ======================================= Attributes ======================================= */
+
 	@Constraints.Required
   private AlmondVariety almondVariety;
+
+  // TODO Change to AlmondSize Size within Almond model.
+  @Constraints.Required
+  private String almondSize;
+
+/* ======================================= Getters ======================================= */
 
   public AlmondVariety getAlmondVariety() {
     return almondVariety;
   }
 
- public void setAlmondVariety(AlmondVariety newVariety) {
+
+  public String getAlmondSize() {
+    return almondSize;
+  }
+
+/* ======================================= Setters ======================================= */
+
+  public void setAlmondVariety(AlmondVariety newVariety) {
     almondVariety = newVariety;
+  }
+
+  public void setAlmondSize(String newSize) {
+    almondSize = newSize;
   }
 
 }
