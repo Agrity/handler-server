c5e677871ee1543d3059a6c9d27507c86921256d
Ryan Davies <ryscot@gmail.com>
2016-07-11 13:35:57 -0700

    Evolution Script Written.

---
+++ conf/evolutions/default/2.sql
@@ -0,0 +1,29 @@
+
+# --- !Ups
+
+
+# Add Management Type, Expiration Time, and Overall Accept/Reject.
+# ================================================================
+
+# Management Service
+alter table if exists offer add column management_service varchar(255) set default "NOT SET";
+
+# Drop then Add Currently Open Column with Different Data Types
+alter table if exists offer drop column if exists offer_currently_open;
+alter table if exists offer add column offer_currently_open integer set default 2;
+
+# Add Expiration Time Column
+alter table if exists offer add column expiration_time timestamp set default CURRENT_TIMESTAMP;
+
+
+
+
+# --- !Downs
+
+
+alter table if exists offer drop column if exists  management_service;
+
+alter table if exists offer drop column if exists offer_currently_open;
+alter table if exists offer add column if exists offer_currently_open boolean default false;
+
+alter table if exists offer drop column if exists expiration_time;
