56e7f5582bb9ab892c2262121cd5abbb0eaaad98
Brian Rossi <brossi15621@gmail.com>
2016-07-12 14:35:25 -0700

    updated dev scripts and partial acceptance link

--- development-scripts/delete-bid.sh
+++ development-scripts/delete-bid.sh
@@ -7,7 +7,7 @@ if ! [[ -z $HANDLER_BID_ID ]]; then
     --header "Content-type: application/json" \
     --header "X-ADMIN-TOKEN: development-use-only" \
     --request DELETE \
-    localhost:9000/admin/bids/$HANDLER_BID_ID
+    localhost:9000/admin/handlerBids/$HANDLER_BID_ID
   echo # Insert Blank Line
 else 
   echo "ERROR: Please provide an bid id to delete."
--- development-scripts/update-bid.sh
+++ development-scripts/update-bid.sh
@@ -8,7 +8,7 @@ if ! [[ -z $HANDLER_BID_ID ]]; then
     --header "X-ADMIN-TOKEN: development-use-only" \
     --request PUT \
     --data '{ "handler_id" : 1, "grower_ids" : [1], "almond_variety" : "NP", "almond_size" : "23/25", "almond_pounds" :  500000,  "price_per_pound" : "2.15", "start_payment_date" : "June 2015", "end_payment_date" : "June 2016", "management_type" : { "type" : "FCFSService", "delay" : 10}, "comment" : "This is an awesome test comment that was changed!", "email_addresses" : "ryscot@gmail.com" }' \
-    localhost:9000/admin/bids/$HANDLER_BID_ID
+    localhost:9000/admin/handlerBids/$HANDLER_BID_ID
   echo # Insert Blank Line
 else
   echo "ERROR: Please provide an bid id to update."
