b23f61d6c780be3234d1a42dc12908651e24b94b
Brian Rossi <brossi15621@gmail.com>
2016-07-13 13:33:26 -0700

    BaseSell and GrowerSeller created
    will now go through and make all the changes from Grower object to
    GrowerSeller object throughout the app to get clean compile. Will not
    mess with routes yet.

---
+++ app/models/BaseSeller.java
@@ -0,0 +1,106 @@
+package models;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.OneToMany;
+import javax.persistence.MappedSuperClass;
+import play.data.validation.Constraints;
+
+
+@MappedSuperClass
+public abstract class BaseSeller extends BaseModel {
+
+  /* ======================================= Attributes ======================================= */
+
+  @Constraints.Required
+  private String firstName;
+
+  @Constraints.Required
+  private String lastName;
+
+  /**
+   * TODO: Change to play email format to use play-mailer plugin
+   *
+   * <a href="https://github.com/playframework/play-mailer/blob/master/README.adoc">Plugin Link</a>
+   */
+  @OneToMany(cascade = CascadeType.ALL)
+  @Constraints.Required
+  public List<EmailAddress> emailAddresses;
+
+  /**
+   * TODO: Change to phone number format, construct own model so that can be consistant.
+   */
+  @OneToMany(cascade = CascadeType.ALL)
+  @Constraints.Required
+  public List<PhoneNumber> phoneNumbers;
+
+  /* ======================================= Attribute Accessors ======================================= */
+
+  public String getFirstName() {
+    return firstName;
+  }
+
+  public String getLastName() {
+    return lastName;
+  }
+
+
+  @JsonIgnore
+  public String getFullName() {
+    return firstName + " " + lastName;
+  }
+
+  @JsonIgnore
+  public List<EmailAddress> getEmailAddresses() {
+    return emailAddresses;
+  }
+
+  public List<String> getEmailAddressStrings() {
+    return getEmailAddresses()
+      .stream()
+      .map(EmailAddress::getEmailAddress)
+      .collect(Collectors.toList());
+  }
+
+  @JsonIgnore
+  public List<PhoneNumber> getPhoneNumbers() {
+    return phoneNumbers;
+  }
+
+  public List<String> getPhoneNumsStrings() {
+    return getPhoneNumbers()
+      .stream()
+      .map(PhoneNumber::getPhoneNumber)
+      .collect(Collectors.toList());
+  }
+
+  /* ======================================= Attribute Setters ======================================= */
+
+  public void setFirstName(String firstName) {
+    this.firstName = firstName;
+  }
+
+  public void setLastName(String lastName) {
+    this.lastName = lastName;
+  }
+
+  public void setPhoneNumbers(List<PhoneNumber> phoneNumbers) {
+    this.phoneNumbers = this.phoneNumbers;
+  }
+
+  public void setEmailAddresses(List<EmailAddress> emails) {
+    emailAddresses = emails;
+  }
+
+  /* ======================================= Member Functions ======================================= */
+
+  @Override
+  public String toString() {
+    // TODO
+    return "(" + id + ") " + getFullName() + " [" + getEmailAddresses().toString() + "]";
+  }
+}
--- app/models/GrowerSeller.java
+++ app/models/GrowerSeller.java
@@ -18,7 +18,7 @@ import models.interfaces.PrettyString;
 import play.data.validation.Constraints;
 
 @Entity
-public class Grower extends BaseModel implements PrettyString {
+public class GrowerSeller extends BaseSeller implements PrettyString {
 
   @ManyToOne
   @Constraints.Required
@@ -27,29 +27,6 @@ public class Grower extends BaseModel implements PrettyString {
   public void setHandler(Handler handler) {
     this.handler = handler;
   }
-
-  @Constraints.Required
-  private String firstName;
-
-  @Constraints.Required
-  private String lastName;
-
-  /**
-   * TODO: Change to play email format to use play-mailer plugin
-   *
-   * <a href="https://github.com/playframework/play-mailer/blob/master/README.adoc">Plugin Link</a>
-   */
-  @OneToMany(cascade = CascadeType.ALL)
-  @Constraints.Required
-  public List<EmailAddress> emailAddresses;
-
-  /**
-   * TODO: Change to phone number format, construct own model so that can be consistant.
-   */
-  @OneToMany(cascade = CascadeType.ALL)
-  @Constraints.Required
-  public List<PhoneNumber> phoneNumbers;
-
 
   // TODO Remove BidResponses and Bidss from Growers if possible.
   @Constraints.Required
@@ -66,56 +43,42 @@ public class Grower extends BaseModel implements PrettyString {
   /* ==================================== Static Functions ==================================== */
 
 
-  public static Finder<Long, Grower> find = new Finder<>(Grower.class);
+  public static Finder<Long, GrowerSeller> find = new Finder<>(GrowerSeller.class);
 
-  public static Grower createGrower(Handler handler, String firstName, String lastName) {
-    Grower grower = new Grower(handler, firstName, lastName, new ArrayList<>(), new ArrayList<>());
-    grower.save();
-    return grower;
-  }
+  // public static GrowerSeller createGrower(Handler handler, String firstName, String lastName) {
+  //   Grower grower = new Grower(handler, firstName, lastName, new ArrayList<>(), new ArrayList<>());
+  //   grower.save();
+  //   return grower;
+  // }
   
 
 
   /* ========== Member Functions ========== */
 
-  public Grower(Handler handler, String firstName, String lastName) {
-    super();
+  // public Grower(Handler handler, String firstName, String lastName) {
+  //   super();
 
-    this.handler = handler;
-    this.firstName = firstName;
-    this.lastName = lastName;
+  //   this.handler = handler;
+  //   this.firstName = firstName;
+  //   this.lastName = lastName;
 
-    this.emailAddresses = new ArrayList<>();
-    this.phoneNumbers = new ArrayList<>();
-  }
+  //   this.emailAddresses = new ArrayList<>();
+  //   this.phoneNumbers = new ArrayList<>();
+  // }
 
-  public Grower(Handler handler, String firstName, String lastName,
+  public GrowerSeller(Handler handler, String firstName, String lastName,
       List<EmailAddress> emailAddresses, List<PhoneNumber> phoneNumbers) {
     super();
 
-    this.firstName = firstName;
-    this.lastName = lastName;
+    setFirstName(firstName);
+    setLastName(lastName);
     this.handler = handler;
-    this.emailAddresses = emailAddresses;
-    this.phoneNumbers = phoneNumbers;
+    setEmailAddresses(emailAddresses);
+    setPhoneNumbers(phoneNumbers);
   }
 
   public Handler getHandler() {
     return handler;
-  }
-
-  public String getFirstName() {
-    return firstName;
-  }
-
-  public String getLastName() {
-    return lastName;
-  }
-
-
-  @JsonIgnore
-  public String getFullName() {
-    return firstName + " " + lastName;
   }
 
   @JsonIgnore
@@ -158,47 +121,6 @@ public class Grower extends BaseModel implements PrettyString {
     return handlerBids;
   }
 
-  @JsonIgnore
-  public List<EmailAddress> getEmailAddresses() {
-    return emailAddresses;
-
-  }
-
-  public List<String> getEmailAddressStrings() {
-    return getEmailAddresses()
-      .stream()
-      .map(EmailAddress::getEmailAddress)
-      .collect(Collectors.toList());
-  }
-
-  @JsonIgnore
-  public List<PhoneNumber> getPhoneNumbers() {
-    return phoneNumbers;
-  }
-
-  public List<String> getPhoneNumsStrings() {
-    return getPhoneNumbers()
-      .stream()
-      .map(PhoneNumber::getPhoneNumber)
-      .collect(Collectors.toList());
-  }
-
-  public void setFirstName(String firstName) {
-    this.firstName = firstName;
-  }
-
-  public void setLastName(String lastName) {
-    this.lastName = lastName;
-  }
-
-  public void setPhoneNumbers(List<PhoneNumber> phoneNumbers) {
-    this.phoneNumbers = this.phoneNumbers;
-  }
-
-  public void setEmailAddresses(List<EmailAddress> emails) {
-    emailAddresses = emails;
-  }
-
   public HandlerBid bidLookupByID(Long bidID) {
     for (HandlerBid handlerBid: getHandlerBids()) {
       if (handlerBid.getId().equals(bidID)) {
@@ -208,18 +130,12 @@ public class Grower extends BaseModel implements PrettyString {
     return null;
   }
 
-  @Override
-  public String toString() {
-    // TODO
-    return "(" + id + ") " + getFullName() + " [" + emailAddresses.toString() + "]";
-  }
-
   public String toPrettyString() {
     StringBuilder builder = new StringBuilder()
         .append("(" + id + ") " + getFullName());
 
     builder.append(" [ ");
-    for (EmailAddress addr : emailAddresses) {
+    for (EmailAddress addr : getEmailAddresses()) {
       builder.append(addr + ", ");
     }
 
@@ -229,6 +145,4 @@ public class Grower extends BaseModel implements PrettyString {
 
     return builder.toString();
   }
-
-  /* ========== Static Functions ========== */
 }
