62b8978bd55b8b446cd4928fff9bce9d3302552a
Brian Rossi <brossi15621@gmail.com>
2016-07-13 14:35:09 -0700

    implemented HandlerSellerService and EbeanHandlerSellerService

---
+++ app/services/EbeanHandlerSellerService.java
@@ -0,0 +1,56 @@
+package services.impl;
+
+import com.avaje.ebean.Model.Finder;
+import java.util.List;
+
+import models.HandlerSeller;
+import models.PhoneNumber;
+
+import services.HandlerSellerService;
+import play.Logger;
+
+public class EbeanHandlerSellerService implements HandlerSellerService {
+
+  private static Finder<Long, HandlerSeller> FINDER = new Finder<>(HandlerSeller.class);
+
+  @Override
+  public List<HandlerSeller> getAll() {
+    return FINDER.all();
+  }
+
+  @Override
+  public HandlerSeller getById(long id) {
+    return FINDER.byId(id);
+  }
+
+  @Override
+  public List<HandlerSeller> getByHandlerSeller(long handlerSellerId) {
+    // TODO Assert Handler Exists, or Return Null
+    return FINDER.where()
+        // TODO Fix this Column Name.
+        .eq("handler_seller_id", handlerSellerId)
+        .findList();
+  }
+
+  @Override
+  public List<HandlerSeller> getByBid(long bidId) {
+    // TODO Assert Grower Exists, or Return Null
+    return FINDER.where()
+        .eq("bids.id", bidId)
+        .findList();
+  }
+
+  public HandlerSeller handlerSellerLookupByPhoneNum(String phoneNum) {
+    List<HandlerSeller> handlerSellers = getAll(); 
+    for (HandlerSeller handlerSeller: handlerSellers) {
+      for (String curPhoneNum: handlerSeller.getPhoneNumsStrings()) {
+        Logger.info("These are the curPhoneNums being looked up: " + curPhoneNum
+                 + "   " + phoneNum + "\n\n");
+        if (curPhoneNum.equals(phoneNum)) {
+          return handlerSeller;
+        }
+      }
+    }
+    return null;
+  }
+}
