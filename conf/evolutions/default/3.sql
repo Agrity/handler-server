193398a3b5f115d508f4552273d8c51744fad814
Ryan Davies <ryscot@gmail.com>
2016-08-09 12:22:08 -0700

    Fixed Response Status

---
+++ conf/evolutions/default/3.sql
@@ -0,0 +1,15 @@
+# Add Grade to Trader Bids
+
+# ================================================================
+# --- !Ups
+
+
+alter table if exists bid_responses drop constraint if exists ck_bid_responses_response_status;
+alter table if exists bid_responses add constraint ck_bid_responses_response_status check (response_status in (0,1,2,3,4,5));
+
+
+# --- !Downs
+
+
+alter table if exists bid_responses drop constraint if exists ck_bid_responses_response_status;
+alter table if exists bid_responses add constraint ck_bid_responses_response_status check (response_status in (0,1,2,3));
