798520f60d3ca84d52df28159c33c2fa65b4c43b
Brian Rossi <brossi15621@gmail.com>
2016-08-10 14:15:08 -0700

    fuck ryan

--- conf/evolutions/default/4.sql
+++ conf/evolutions/default/4.sql
@@ -3,12 +3,12 @@
 # ================================================================
 # --- !Ups
 
-alter table if exists bids drop constraint if exists ck_bids_almond_variety;
-alter table if exists bids alter column if exists almond_variety type varchar(255);
+alter table if exists bids drop constraint ck_bids_almond_variety;
+alter table if exists bids alter column almond_variety type varchar(255);
 
 
 # --- !Downs
 
 
-alter table if exists bids alter column if exists almond_variety type varchar(2);
+alter table if exists bids alter column almond_variety type varchar(2);
 alter table if exists bids add constraint ck_bids_almond_variety check (almond_variety in ('PD','FR','PR','MI','MT','PL','BT','SN','NP','CR'));
