6d4abf93aa9eff00ce089d94bc13d4090a53dbb3
Brian Rossi <brossi15621@gmail.com>
2016-08-10 15:33:04 -0700

    fixes

--- development-scripts/new-grower.sh
+++ development-scripts/new-grower.sh
@@ -5,7 +5,7 @@ if [[ -z $1 ]]; then
     --header "Content-type: application/json" \
     --header "X-ADMIN-TOKEN: development-use-only" \
     --request POST \
-    --data '{ "handler_id" : 1, "first_name" : "F_NAME", "last_name" : "L_NAME", "email_address" : "test@test.com", "phone_number" : "5551231234"}' \
+    --data '{ "handler_id" : 10, "first_name" : "F_NAME", "last_name" : "L_NAME", "email_address" : "brossi@stanford.edu", "phone_number" : "5551231234"}' \
     localhost:9000/admin/growers
 else
   echo "ERROR: do not use any arguements."
